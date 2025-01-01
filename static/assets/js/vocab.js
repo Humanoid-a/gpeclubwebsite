d1b8adae9f3c0eb44130d89391e047b8497c53c8
BroWo1 <zhangyang.li@icloud.com>
2025-01-02 02:17:22 +0800

    vocab0.3

--- static/assets/js/vocab.js
+++ static/assets/js/vocab.js
@@ -2,8 +2,44 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchData();
     setupEventListeners();
 });
+var knownWords = [];
+var unknownWords = [];
 
 let vocabData = [];  // Initialize as an array
+if (getCookie('knownWords') !== null) {
+    let knownWordsCookie = getCookie('knownWords');
+    knownWords = knownWordsCookie ? JSON.parse(knownWordsCookie) : [];
+    if (!Array.isArray(knownWords)) {
+        knownWords = [];
+    }
+} else {
+    knownWords = [];
+}
+console.log(knownWords);
+if (getCookie('unknownWords') !== null) {
+    let unknownWordsCookie = getCookie('unknownWords');
+    unknownWords = unknownWordsCookie ? JSON.parse(unknownWordsCookie) : [];
+    if (!Array.isArray(unknownWords)) {
+        unknownWords = [];
+    }
+} else {
+    unknownWords = [];
+}
+console.log(unknownWords);
+
+function setCookie(name, value) {
+    document.cookie = name + "=" + (value || "") + "; path=/";
+}
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
 
 function fetchData() {
     // Fetching from Django View
@@ -47,16 +83,32 @@ function setupEventListeners() {
     const flashcard = document.getElementById('flashcard');
     const prevBtn = document.getElementById('prevBtn');
     const nextBtn = document.getElementById('nextBtn');
+    const known = document.getElementById('known');
+    const unknown = document.getElementById('unknown');
     const random = document.getElementById('random');
+    const practiceUnknown = document.getElementById('practiceUnknown');
+    const clear = document.getElementById('clear');
+    let modeUnknown = practiceUnknown.checked;
     let israndom = random.checked;
     let currentIndex = 0;
+    let unknownIndex = 0;
 
     random.addEventListener('change', () => {
         israndom = random.checked;
     });
+    practiceUnknown.addEventListener('change', () => {
+        modeUnknown = practiceUnknown.checked;
+    });
 
     flashcard.addEventListener('click', () => {
         flashcard.classList.toggle('flipped');
+    });
+
+    clear.addEventListener('click', () => {
+        knownWords = [];
+        unknownWords = [];
+        setCookie('knownWords', JSON.stringify(knownWords));
+        setCookie('unknownWords', JSON.stringify(unknownWords));
     });
 
     prevBtn.addEventListener('click', () => {
@@ -72,17 +124,62 @@ function setupEventListeners() {
 
     nextBtn.addEventListener('click', () => {
         if (!israndom) {
-            if (currentIndex < vocabData.length - 1) {
-                currentIndex++;
-                displayFlashcard(currentIndex);
-            } else {
-                alert('You have reached the last flashcard.');
+            if (!modeUnknown){
+                if (currentIndex < vocabData.length - 1) {
+                    currentIndex++;
+                    if (knownWords !== null){
+                        while(knownWords.includes(currentIndex)){
+                        currentIndex++;
+                        }
+                    }
+                    displayFlashcard(currentIndex);
+                } else {
+                    alert('You have reached the last flashcard.');
+                }
+            }else {
+                if (unknownIndex < unknownWords.length - 1) {
+                    unknownIndex++;
+                    displayFlashcard(unknownWords[unknownIndex]);
+                } else {
+                    alert('You have reached the last flashcard.');
+                }
             }
-        } else {
-            currentIndex = Math.floor(Math.random() * vocabData.length);
-            displayFlashcard(currentIndex);
+        }else{
+            if(!modeUnknown){
+                currentIndex = Math.floor(Math.random() * vocabData.length);
+                if (knownWords !== null) {
+                    while (knownWords.includes(currentIndex)) {
+                        currentIndex = Math.floor(Math.random() * vocabData.length);
+                    }
+                }
+                displayFlashcard(currentIndex);
+            }else{
+                unknownIndex = Math.floor(Math.random() * unknownWords.length);
+                displayFlashcard(unknownWords[unknownIndex]);
+            }
         }
     });
+
+    known.addEventListener('click', () => {
+        if(!knownWords.includes(currentIndex)){
+            knownWords.push(currentIndex);
+            console.log(knownWords);
+            setCookie('knownWords', JSON.stringify(knownWords));
+            if(unknownWords !== null && unknownWords.includes(currentIndex)){
+                unknownWords = unknownWords.filter(index => index !== currentIndex);
+                setCookie('unknownWords', JSON.stringify(unknownWords));
+            }
+        }
+    });
+
+    unknown.addEventListener('click', () => {
+        if(!unknownWords.includes(currentIndex)){
+            unknownWords.push(currentIndex);
+            console.log(unknownWords);
+            setCookie('unknownWords', JSON.stringify(unknownWords));
+        }
+    });
+
 }
 
 function displayFlashcard(index) {
